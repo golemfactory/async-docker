05b2c671e6298b17513d297470be32093cb0e563
destruktiw <hubert.banaszewski@golem.network>
2019-03-15 17:54:20 +0100

    Fix SSL to compiling state

--- src/communicate/ssl_tcp_docker.rs
+++ src/communicate/ssl_tcp_docker.rs
@@ -2,23 +2,24 @@
 
 extern crate hyper_openssl;
 extern crate openssl;
-use self::{
-    hyper_openssl::HttpsConnector,
-    openssl::{
-        ssl::{SslConnectorBuilder, SslMethod},
-        x509::X509Builder,
-    },
-};
+use self::hyper_openssl::HttpsConnector;
+use self::openssl::ssl::SslMethod;
 
-use self::openssl::ssl::SslContextBuilder;
-use communicate::docker::{Docker, DockerApi};
+use communicate::docker::Docker;
+use communicate::docker::DockerApi;
 use errors::Result;
-use hyper::{client::HttpConnector, Body, Client, Uri};
-use std::{env, path::Path, sync::Arc};
+use hyper::client::HttpConnector;
+use hyper::Client;
+use hyper::Uri;
+use std::env;
+use std::path::Path;
+use std::sync::Arc;
 use transport::interact::Interact;
 use Error;
+use self::openssl::ssl::SslConnector;
+use self::openssl::ssl::SslFiletype;
 
-pub type TcpSSLDocker = Docker<HttpsConnector<HttpConnector>>;
+pub(crate) type TcpSSLDocker = Docker<HttpsConnector<HttpConnector>>;
 
 const THREADS: usize = 1;
 
@@ -29,14 +30,12 @@ impl Docker<HttpsConnector<HttpConnector>> {
         let cert = &format!("{}/cert.pem", certs);
         let key = &format!("{}/key.pem", certs);
 
-        // https://github.com/hyperium/hyper/blob/master/src/net.rs#L427-L428
-        let context = SslContextBuilder::new(SslMethod::tls())
+        let mut connector = SslConnector::builder(SslMethod::tls())
             .expect("Error during ssl connection preparation");
-        let mut connector = SslConnectorBuilder { context }.build();
 
         connector.set_cipher_list("DEFAULT")?;
-        connector.set_certificate_file(&Path::new(cert), X509Builder::new())?;
-        connector.set_private_key_file(&Path::new(key), X509Builder::new())?;
+        connector.set_certificate_file(&Path::new(cert), SslFiletype::PEM)?;
+        connector.set_private_key_file(&Path::new(key), SslFiletype::PEM)?;
 
         if let Some(_) = env::var("DOCKER_TLS_VERIFY").ok() {
             let ca = &format!("{}/ca.pem", certs);
